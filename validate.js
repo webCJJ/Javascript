2a7da35d9b3eeb3d31b80e5a1aec5863e92e590d
cjj <1228424102@qq.com>
2017-08-23 17:37:56 +0800

    validate 插件

---
+++ validate.js
@@ -0,0 +1,16 @@
+/**
+ * Created by cjj on 2017/8/23.
+ */
+
+function CjjValidate(e,reg,text){
+    this.e = e;
+    this.reg = reg;
+    this.text = text;
+}
+CjjValidate.prototype = {
+    constructor : CjjValidate,
+    CjjFocus : function(){
+
+    }
+
+}
